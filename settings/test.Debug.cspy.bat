b6dc0e048c11b72490ad28b057752fcbdc5b475b
전상현 <opark001@gmail.com>
2025-12-20 11:52:53 +0900

    bug fix

--- settings/test.Debug.cspy.bat
+++ settings/test.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\pnu1\Desktop\tue01\project_test_2\project_test\settings\test.Debug.general.xcl" --backend -f "C:\Users\pnu1\Desktop\tue01\project_test_2\project_test\settings\test.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\pnu3\Desktop\deskproject\Embedded_System_TermProject\settings\test.Debug.general.xcl" --backend -f "C:\Users\pnu3\Desktop\deskproject\Embedded_System_TermProject\settings\test.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\pnu1\Desktop\tue01\project_test_2\project_test\settings\test.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\pnu1\Desktop\tue01\project_test_2\project_test\settings\test.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\pnu3\Desktop\deskproject\Embedded_System_TermProject\settings\test.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\pnu3\Desktop\deskproject\Embedded_System_TermProject\settings\test.Debug.driver.xcl" 
 
 @echo off 
 :end
--- settings/test.Debug.cspy.ps1
+++ settings/test.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\pnu1\Desktop\tue01\project_test_2\project_test\settings\test.Debug.general.xcl" --backend -f "C:\Users\pnu1\Desktop\tue01\project_test_2\project_test\settings\test.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\pnu3\Desktop\deskproject\Embedded_System_TermProject\settings\test.Debug.general.xcl" --backend -f "C:\Users\pnu3\Desktop\deskproject\Embedded_System_TermProject\settings\test.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\pnu1\Desktop\tue01\project_test_2\project_test\settings\test.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\pnu1\Desktop\tue01\project_test_2\project_test\settings\test.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\pnu3\Desktop\deskproject\Embedded_System_TermProject\settings\test.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\pnu3\Desktop\deskproject\Embedded_System_TermProject\settings\test.Debug.driver.xcl" 
 }
